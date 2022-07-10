98644b5c157ffc349d84f342e177a719205b00d9
johnjake <johnjake.talledo2000@gmail.com>
2022-07-11 06:33:02 +0800

    fragment base

--- app/src/main/java/app/scanner/calc/bases/BaseActivity.kt
+++ app/src/main/java/app/scanner/calc/bases/BaseActivity.kt
@@ -3,11 +3,11 @@ package app.scanner.calc.bases
 import android.os.Bundle
 import android.view.LayoutInflater
 import androidx.viewbinding.ViewBinding
-import app.scanner.calc.hilt.ActivityEntry
+import app.scanner.calc.hilt.HiltActivityEntry
 
 abstract class BaseActivity<T : ViewBinding>(
     private val setUpViewBinding: (LayoutInflater) -> T
-) : ActivityEntry() {
+) : HiltActivityEntry() {
     lateinit var binding: T
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
