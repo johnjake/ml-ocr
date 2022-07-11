41d3fe3a0a64fac9fd4b3bd2e9cad828c0f831b6
johnjake <johnjake.talledo2000@gmail.com>
2022-07-11 12:22:18 +0800

    add required

--- app/src/main/java/app/scanner/calc/features/MainActivity.kt
+++ app/src/main/java/app/scanner/calc/features/MainActivity.kt
@@ -1,10 +1,12 @@
 package app.scanner.calc.features
 
+import android.net.Uri
 import android.view.View
 import app.scanner.calc.bases.BaseActivity
 import app.scanner.calc.databinding.ActivityMainBinding
 import app.scanner.domain.filesystem.FileGallery
 import app.scanner.domain.manager.CameraBuilder
+import app.scanner.domain.toast
 
 class MainActivity : BaseActivity<ActivityMainBinding>
     (ActivityMainBinding::inflate) {
@@ -21,6 +23,7 @@ class MainActivity : BaseActivity<ActivityMainBinding>
     override fun onResume() {
         super.onResume()
         if(openGallery.init) {
+            require(openGallery.getUri()!= null) { toast("Invalid image file!") }
             binding.previewImage.apply {
                 visibility = View.VISIBLE
                 setImageURI(openGallery.getUri())
