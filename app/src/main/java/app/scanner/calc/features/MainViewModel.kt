a92ecb78bc2d6f5be12aeadcd27a78d4703bec2a
johnjake <johnjake.talledo2000@gmail.com>
2022-07-16 14:23:15 +0800

    modify BaseSate

--- app/src/main/java/app/scanner/calc/features/MainViewModel.kt
+++ app/src/main/java/app/scanner/calc/features/MainViewModel.kt
@@ -1,16 +1,11 @@
 package app.scanner.calc.features
 
-import android.graphics.Bitmap
-import androidx.lifecycle.viewModelScope
 import app.scanner.calc.bases.BaseState
 import app.scanner.calc.bases.BaseViewModel
 import app.scanner.domain.repository.ReaderAction
-import com.google.mlkit.vision.text.Text
-import com.google.mlkit.vision.text.TextRecognizer
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharedFlow
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -20,18 +15,6 @@ class MainViewModel @Inject constructor(
     private val ocrFlow: MutableSharedFlow<BaseState> = MutableSharedFlow(replay = 1)
     val ocrState: SharedFlow<BaseState> = ocrFlow
 
-    fun recognizeExpression(recognizer: TextRecognizer, bitmap: Bitmap) {
-        viewModelScope.launch {
-            ocrReader.readText(recognizer, bitmap).let { text ->
-                when {
-                    text.text.isEmpty() -> ocrFlow.emit(BaseState.OnReadFailed())
-                    else -> ocrFlow.emit(BaseState.OnReadSuccess(text))
-                }
-            }
-        }
-    }
-
     suspend fun getMathExpression(expression: String): String = ocrReader.getExpression(expression)
     suspend fun solveMathEquation(mathExpression: String): String = ocrReader.solveMathEquation(mathExpression)
-    suspend fun getProcessResult(recognizeText: Text): String = ocrReader.processResult(recognizeText)
 }
